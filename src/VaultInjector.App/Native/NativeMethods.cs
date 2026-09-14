ba15f27bda557461c343e420f80a22185ec7df3e
Maciej Chlebny <chlebny59@gmail.com>
2026-09-14 16:06:19 +0200

    System Kernel32.dll

--- src/VaultInjector.App/Native/NativeMethods.cs
+++ src/VaultInjector.App/Native/NativeMethods.cs
@@ -26,7 +26,7 @@ internal static class NativeMethods
     [DllImport("user32.dll")]
     public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
-    [DllImport("user32.dll")]
+    [DllImport("kernel32.dll")]
     public static extern uint GetCurrentThreadId();
 
     [DllImport("user32.dll")]
